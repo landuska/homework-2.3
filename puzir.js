04771a4b43b89fbbdc1eec31fff4b54db7400fcf
Landysh Khusaenova <landysh.kg@gmail.com>
2023-10-25 00:11:23 +0200

    Calculate puzir

--- puzir.js
+++ puzir.js
@@ -2,7 +2,7 @@ let scoreStudent = [12, 45, 78, 32, 56, 89, 24, 68, 91, 42, 73, 19, 60, 37, 51, 
 console.log(scoreStudent)
 for (let i = 0; i < scoreStudent.length-1; i++) {
     for (let j = 0; j < scoreStudent.length-i-1; j++){
-        if (scoreStudent[j] > scoreStudent [j+1]) {
+        if (scoreStudent[j] < scoreStudent [j+1]) {
             let temp = scoreStudent [j];
             scoreStudent[j] = scoreStudent [j+1];
             scoreStudent [j+1] = temp;
